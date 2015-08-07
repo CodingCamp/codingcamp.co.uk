3667893481c83265263fa1ae69f177b4e65a43f6
“Jessica <wesika@hotmail.com>
2015-08-07 12:05:39 +0100

    Week 1 and 2 work

---
+++ Week2/sjbpwork-of-art/js/index.js
@@ -0,0 +1,15 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [85, 100, 67];
+var blue = [172, 100, 67];
+var purple = [280, 50, 60];
+var yellow = [55,100,67];
+
+var myName = "Sophie.J";
+var letterColors = [purple,red,green,yellow, blue];
+bubbleShape = "circle";
+
+
+
+drawName(myName, letterColors);
+bounceBubbles();
